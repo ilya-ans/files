8865877ad44565929bb36960c5438304d0dea8f6
ilya-ans <ianisimov982@gmail.com>
2024-12-13 20:03:22 +0700

    add images

--- labs/files-lab2/scripts/script-task1.js
+++ labs/files-lab2/scripts/script-task1.js
@@ -1,6 +1,46 @@
+function is_wrong_string(userString){
+	var stringOfCorrectValues = "0123456789. ";
+	var stringOfCorrectValuesLength = stringOfCorrectValues.length;
+	var userStringLength = userString.length;
+	var numbersCounter = 0;
+	var userStringCounter = 0;
+	var stringOfCorrectValuesCounter = 0;
+	var isSymbolWrong = true;
+	
+	while (userStringCounter < userStringLength){
+		while (stringOfCorrectValuesCounter < stringOfCorrectValuesLength){
+			if (userString[userStringCounter] === stringOfCorrectValues[stringOfCorrectValuesCounter]){
+				isSymbolWrong = false;
+				break;
+			}
+			stringOfCorrectValuesCounter++;
+		}
+		
+		if (isSymbolWrong){
+			return true;
+		}
+		
+		stringOfCorrectValuesCounter = 0;
+		userStringCounter++;
+		isSymbolWrong = true;
+	}
+	return false;
+}
+
+
 function calculate_s(a){
+	/*
+	
+	z = (typeof 1 == 'number');
+	console.log(z);
+
+	
+	if (is_wrong_string(a)){
+		return "Проверьте введенные данные!";
+	}
+	*/
 	var square_s;
-	square_s = a * a;
+	square_s = Number(a) * Number(a);
 	return "Площадь равна " + square_s + " сантиметров";
 }
 
@@ -9,6 +49,6 @@ var a_side_submit = document.querySelector(".a_side_submit");
 var write_result_s = document.querySelector(".write_result_s");
 
 a_side_submit.onclick = function () {
-	write_result_s.textContent = calculate_s(Number(read_a_side_value.value));
+	write_result_s.textContent = calculate_s(read_a_side_value.value);
 }
 
--- labs/files-lab2/scripts/script-task2.js
+++ labs/files-lab2/scripts/script-task2.js
@@ -1,7 +1,7 @@
 function calculate_tons(kilos, koef) {
 	var tons;
-	tons = Math.floor(kilos/koef);
-	return "В " + kilos + " килограммах содержится " + tons + " тонн";
+	tons = Math.floor(Number(kilos)/koef);
+	return "В " + Number(kilos) + " килограммах содержится " + tons + " тонн";
 }
 
 var read_kilos_value = document.querySelector(".read_kilos_value");
@@ -10,6 +10,6 @@ var write_result_tons = document.querySelector(".write_result_tons");
 const kilos_in_tons_koef = 1000;
 
 kilos_submit.onclick = function () {
-	write_result_tons.textContent = calculate_tons(Number(read_kilos_value.value), kilos_in_tons_koef);
+	write_result_tons.textContent = calculate_tons(read_kilos_value.value, kilos_in_tons_koef);
 }
 
